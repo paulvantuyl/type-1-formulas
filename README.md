fb42c91ae948525fbd4f73f3b1d8b64a1a54715e
Paul Van Tuyl <paulvantuyl@gmail.com>
2016-01-31 20:28:20 -0700

    Updates
    Add link to the sheet template and update formulas to clean up.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # type-1-formulas
-Formulas for finding insulin dosage requirements with type 1 diabetes.
+[Spreadsheet calculator](https://docs.google.com/spreadsheets/d/1LGaah2VlM_EmLH81urx0RWHukKQMjEQO9tSyYZpYbe4/edit?usp=sharing) for finding insulin dosage requirements for people with type 1 diabetes.
--- sheets-forumlas.js
+++ sheets-forumlas.js
@@ -13,34 +13,12 @@
     D: Calculated units column for fast-acting insulin
 */
 
-
-// Range adjustments
+// Current calculator forumula
 =
 IF( AND(ISBLANK(B2)=true,ISBLANK(C2)=true) , "",
 IF( B2>Settings!C$1 , ((B2-Settings!C$1)/Settings!C$2)+(C2/Settings!C$3),
 IF( B2<Settings!B$1 , ((B2-Settings!B$1)/Settings!C$2)+(C2/Settings!C$3),
 IF( AND(B2>=Settings!B$1,B2<=Settings!C$1) , C2/Settings!C$3))))
 
-// Range test
-=
-IF( (B2>Settings!C$1) , "it's high",
-IF( (B2<Settings!B$1) , "it's low",
-IF( AND(B2>=Settings!B$1,B2<=Settings!C$1) , "it's just right")))
-
-
-// Trying to make a formula for leaving blank cells when there's no data
-=
-IF( (B2>Settings!C$1) , ((B2-Settings!C$1)/Settings!C$2+(C2/Settings!C$3)),
-IF( (B2<Settings!B$1) , ((B2-Settings!B$1)/Settings!C$2)+(C2/Settings!C$3)),
-IF( ISBLANK(B2) , "blank"))
-
-=
-IF( (B2>Settings!C$1) , ((B2-Settings!C$1)/Settings!C$2+(C2/Settings!C$3)),
-IF( (B2<Settings!B$1) , ((B2-Settings!B$1)/Settings!C$2)+(C2/Settings!C$3)),
-"blank")
-
-// Nested blank test thingy
-=
-IF( AND(ISBLANK(B2)=true,ISBLANK(C2)=true) , "bothblank",
-IF( OR(ISBLANK(B2)=true,ISBLANK(C2)=true) , "atleastoneblank",
-IF( AND(ISBLANK(B2)=false,ISBLANK(C2)=false) , "neitherblank")))
+// Old formula
+=IF( (B2>Settings!C$1) , ((B2-Settings!C$1)/Settings!C$2+(C2/Settings!C$3)) , (((B2-Settings!B$1)/Settings!C$2)+(C2/Settings!C$3)))
